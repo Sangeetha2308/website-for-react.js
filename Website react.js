e14cfcc4c9c1d94c2b836c3ae69ae196abe96a72
Sangeetha2308 <127321047+Sangeetha2308@users.noreply.github.com>
2023-04-04 17:02:10 +0530

    Create Website react.js

---
+++ Website react.js
@@ -0,0 +1,82 @@
+INTRODUCTION:
+▪ React is a popular Javascript library used for building user interfaces (Uls) in web applications. React is widely used in web development due to its fast rendering capabilities and ability to handle complex UI components
+
+Ul for Reacs refers to the visual interface of the web application built using React. UI for React is created using a combination of HTML, CSS. and JavaScrips.React provides developersReact (known as react.js) is a free and open for frontend 
+JavaScript library for building user interfaces based on 
+components.
+▪ It is maintained by Meta and a community of individual 
+developers and companies.
+▪ React can be used as a base in the development of single-page, 
+mobile, or server-rendered applications with frameworks like 
+Next.js
+▪ ReactJS provides minimal and solid feature set to kick-start a web 
+application.
+▪ It can be used to develop small applications as well as big, 
+complex applications.
+▪ The main objective of ReactJS is to develop User Interfaces (UI) 
+that improves the speed of the apps. It uses virtual DOM 
+(JavaScript object), which improves the performance of the app. 
+The JavaScript virtual DOM is faster than the regular DOM.
+▪ In React, you develop your applications by creating reusable 
+components that you can think of as independent Lego blocks. 
+These components are individual pieces of a final interface, 
+which, when assembled, form the application’s entire user 
+interface. 
+▪ The main objective of ReactJS is to develop User Interfaces (UI) 
+that improves the speed of the apps. It uses virtual DOM 
+(JavaScript object), which improves the performance of the app. 
+The JavaScript virtual DOM is faster than the regular DOM.
+▪ In React, you develop your applications by creating reusable 
+components that you can think of as independent Lego blocks.
+ 
+HISTORY:
+▪ React was created by Jordan Walke, a software engineer at 
+Meta, who released an early prototype of React called "FaxJS".
+▪ He was influenced by XHP, an HTML component library for 
+PHP. It was first deployed on Facebook's News Feed in 2011 
+and later on Instagram in 2012.
+LIST OF REACT LIBRARIES:
+▪ React router
+▪ Create-react-app
+▪ Ant-design
+▪ Tailwind UI
+▪ Semantic UI react
+▪ React bootstrap
+▪ Fabric react
+▪ Styled components
+▪ React DnD
+▪ Onsen UI
+▪ Rebass
+▪ Material UI
+▪ React-motion
+▪ React spinner
+ADVANTAGES:
+▪ It is composable.
+▪ It is declarative.
+▪ Write once, and learn anywhere.
+▪ It is simple.
+▪ SEO friendly.
+▪ Fast, efficient, and easy to learn.
+▪ It guarantees stable code.
+
+▪ It is backed by a strong community.
+LIMITATIONS:
+▪ ReactJS components are difficult to reuse in complex 
+interactive web projects.
+▪ ReactJS’s Virtual DOM algorithm is time-consuming and 
+imprecise.
+▪ ReactJS’s HTML templates are neither complete nor 
+powerful.
+▪ ReactJS requires complicated asynchronous programming 
+while communicating with the server.
+
+
+APPLICATIONS:
+▪ Snapchat
+.Instagram
+▪ Facebook
+▪ Yahoo
+▪ Myntra
+▪ Netflix
+▪ Dropbox
+▪ Vivaldi
