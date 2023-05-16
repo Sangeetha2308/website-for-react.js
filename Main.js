b8901114822aeb3cd66388a93501a6107cd4175a
Sangeetha2308 <127321047+Sangeetha2308@users.noreply.github.com>
2023-05-16 13:35:20 +0530

    Main.js

---
+++ Main.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  stories: ['../src/**/*.stories.tsx'],
+
+  addons: [
+
+    '@storybook/preset-create-react-app',
+
+    '@storybook/addon-actions',
+
+    '@storybook/addon-knobs/register'
+
+  ]
+
+};
